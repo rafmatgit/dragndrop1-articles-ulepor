7842aeb9d8c5ddd21415793f0ed5af8eb163482c
DonRafal <rafalmatusik1@gmail.com>
2021-03-20 15:31:10 +0100

    2 files modified

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# dragndrop1-articles-ulepor
+# Przeprogramowani.pl - Opanuj JavaScript
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dragndrop1-articles-ulepor)
+Moduł II. - Przeglądarka bez tajemnic
+
+## Ćwiczenie nr. 10 dragNDrop
+
+### Jak zacząć
+
+Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+
+### Cel ćwiczenia
+
+Możliwość przeciągania elementów to intuicyjny sposób manipulowania elementami.
+
+Spraw aby:
+1. było możliwe "przeciągnięcie" kart artykułów z sekcji "Drafts" do "Published" i odwrotnie.
+2. daj użytkownikowi możliwość kontroli nad kolejnością artykułów w obrębie sekcji
+
+### Punkty bonusowe
+
+- I tak jest dość trudno ;)
--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+
+const items = document.querySelectorAll("article");
+let dragged;
+
+items.forEach(item => {
+  item.addEventListener("drag", () => {});
+  item.addEventListener("dragstart", function drag() {
+    dragged = this;
+  });
+  document.addEventListener("dragend", () => {});
+  document.addEventListener("dragover", e => e.preventDefault());
+  document.addEventListener("dragenter", () => {});
+  document.addEventListener("dragleave", () => {});
+  document.addEventListener("drop", e => {
+    e.preventDefault();
+    if (e.target.tagName === "MAIN") {
+      e.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      e.target.append(dragged);
+      if (e.pageY <= 250) {
+        e.target.prepend(dragged);
+      }
+    }
+  });
+});
